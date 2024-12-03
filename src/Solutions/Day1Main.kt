62e2554df7df9fb49237805e7c9f1a17114c7a91
Thomas Trollope <124225201+Thomas-T99@users.noreply.github.com>
2024-12-04 10:29:22 +1300

    Minor errors fixed

--- src/Solutions/Day1Main.kt
+++ src/Solutions/Day1Main.kt
@@ -4,19 +4,19 @@ import java.io.File
 import java.util.*
 
 
-private val leftList: MutableList<Int> = mutableListOf();
-private val rightList: MutableList<Int> = mutableListOf();
-private val rightMap: MutableMap<Int, Int> = mutableMapOf();
+private val leftList: MutableList<Int> = mutableListOf()
+private val rightList: MutableList<Int> = mutableListOf()
+private val rightMap: MutableMap<Int, Int> = mutableMapOf()
 
 private fun day1Main() {
-    readFile("C:\\Users\\thoma\\IdeaProjects\\Advent of Code 2024\\Inputs\\Day1Input");
+    readFile("C:\\Users\\thoma\\IdeaProjects\\Advent of Code 2024\\Inputs\\Day1Input")
     part1()
     part2()
 }
 
 private fun readFile (fileName: String){
     File(fileName).forEachLine {
-        val scan: Scanner = Scanner(it)
+        val scan = Scanner(it)
         leftList.add(scan.nextInt())
         val right = scan.nextInt()
         rightList.add(right)
@@ -40,7 +40,7 @@ private fun part1(){
 }
 
 private fun part2(){
-    var total = 0;
+    var total = 0
     for (i in leftList) {
         if (rightMap.contains(i)){
             total += (rightMap[i]!! * i)
