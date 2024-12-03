629684b1aa634c837a135df53e3902592abde22c
Thomas Trollope <124225201+Thomas-T99@users.noreply.github.com>
2024-12-03 14:03:24 +1300

    Day 1 Completed

--- src/day1/Day1Main.kt
+++ src/day1/Day1Main.kt
@@ -1,4 +1,54 @@
 package day1
 
-class Day1Main {
+import java.io.File
+import java.util.*
+
+
+private val leftList: MutableList<Int> = mutableListOf();
+private val rightList: MutableList<Int> = mutableListOf();
+private val rightMap: MutableMap<Int, Int> = mutableMapOf();
+
+private fun main() {
+    readFile("C:\\Users\\thoma\\IdeaProjects\\Advent of Code 2024\\src\\day1\\Day1Input");
+    part1()
+    part2()
+}
+
+private fun readFile (fileName: String){
+    File(fileName).forEachLine {
+        val scan: Scanner = Scanner(it)
+        leftList.add(scan.nextInt())
+        val right = scan.nextInt()
+        rightList.add(right)
+        if(rightMap.containsKey(right)){
+            val count = rightMap[right]
+            rightMap[right] = count!! + 1
+        } else {
+            rightMap[right] = 1;
+        }
+    }
+}
+
+private fun part1(){
+    leftList.sort();
+    rightList.sort();
+    var total = 0;
+    for (i in 0 until leftList.size) {
+        total += Math.abs(leftList[i] - rightList[i]);
+    }
+    System.out.println("Part 1: " + total +"n");
+}
+
+private fun part2(){
+    var total = 0;
+    for (i in leftList) {
+        if (rightMap.contains(i)){
+            total += (rightMap[i]!! * i)
+        }
+    }
+    System.out.println("Part 2: " + total);
+}
+
+fun main(args: Array<String>) {
+    main();
 }
