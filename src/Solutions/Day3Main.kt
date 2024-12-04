515a756734d4bb1879ce5c41808740491b89e2e1
Thomas Trollope <124225201+Thomas-T99@users.noreply.github.com>
2024-12-04 16:03:07 +1300

    Completed Day 3 Part 2

--- src/Solutions/Day3Main.kt
+++ src/Solutions/Day3Main.kt
@@ -1,8 +1,6 @@
 package Solutions
 
 import java.io.File
-import java.util.*
-import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 private val fileName = "C:\\Users\\thoma\\IdeaProjects\\Advent of Code 2024\\Inputs\\Day3Input"
@@ -39,7 +37,36 @@ private fun part1(){
 }
 
 private fun part2(){
-
+    val mulPat = Pattern.compile("mul[(][0-9]{1,3},[0-9]{1,3}[)]|(do[(][)])|(don't[(][)])")
+    val listOfMul: MutableList<String> = mutableListOf()
+    val matcher = mulPat.matcher(input)
+    while (matcher.find()) {
+        listOfMul.add(matcher.group())
+    }
+    var total = 0
+    var on = true
+    for(mul in listOfMul) {
+        if(on){
+            if(mul == "do()"){
+                on = true
+            } else if(mul == "don't()"){
+                on = false
+            } else {
+                val numPat = Pattern.compile("[0-9]{1,3}")
+                val matcher = numPat.matcher(mul)
+                matcher.find()
+                val no1 = Integer.parseInt(matcher.group())
+                matcher.find()
+                val no2 = Integer.parseInt(matcher.group())
+                total += no1 * no2
+            }
+        } else {
+            if(mul == "do()"){
+                on = true
+            }
+        }
+    }
+    println("Part 2: $total")
 }
 
 fun main(args: Array<String>) {
